db5f5a7a058a5a43e7b6bc8159dd30d1113e46d9
TrungM8 <trungm8fordev@gmail.com>
2024-12-17 15:32:21 +0700

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Trung" };
+  const loggedIn = {
+    firstName: "Trung",
+    lastName: "Le",
+    email: "trungm8fordev@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -21,7 +26,9 @@ const Home = () => {
             totalCurrentBalance={10000.5}
           />
         </header>
+        Recent Transactions
       </div>
+      <RightSideBar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
